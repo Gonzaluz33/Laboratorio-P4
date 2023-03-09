e325e35903c7e5a60d2ed1a230540d39b459039c
Gonzaluz33 <gonzaluz2011@gmail.com>
2023-03-09 09:53:12 -0300

    clase objeto terminada

--- Laboratorio 0/ClaseObjetoAbstract.cpp
+++ Laboratorio 0/ClaseObjetoAbstract.cpp
@@ -3,14 +3,21 @@
 #include <cmath>
 using namespace std;
 
+enum Estado : string {Nuevo, BienConservado, Roto};
+
 class Objeto{
     private:
+            bool estaPrestado;
             string nombre;
             int anioComprado;
+            Ninio* prestado;
+            Estado estadoObjeto;
     public:
         virtual string toString() = 0;
+
 };
 
 int main(){
+
     return 0;
 }
